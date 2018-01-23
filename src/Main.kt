3c2e74be809e95b620ef46b809f7fbcdab68f457
pjo901018 <pjo901018@korea.ac.kr>
2018-01-23 09:31:14 +0900

    flatMap, count() run

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,17 @@
+import java.util.stream.Stream
+
 fun main(args: Array<String>) {
-    print("hello kotlin")
+    println("hello kotlin")
+
+    val intRange: IntRange = 1..100
+    intRange.flatMap { it -> listOf(it, it+1, it+2) }
+            .forEach(::println)
+
+    println("hello world")
+
+    val helloworld = listOf("hello", "world")
+    helloworld.flatMap { it -> it.split("") }
+            .count()
+            .run { println(this) }
+    "hello".split("").count().run { println(this) }
 }
